9baad3f54470ca8f8a5d8016439073dd16cdc59a
Nayan Suter <nayankbps@gmail.com>
2025-05-16 19:45:20 +0600

    first commit with vite-router-tailwind-daisy

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='max-w-[1400px] mx-auto min-h-screen border-4 border-teal-400' >
+      <h1 className='text-4xl'>This is a vite project</h1>
+      <h1 className='text-3xl'>Installed React Router</h1>
+      <h1 className='text-2xl'>Installed and configured TailwindCSS</h1>
+      <h1 className='text-xl'>Installed and configured DaisyUI </h1>
+      
+    </div>
+  )
+}
+
+export default App
