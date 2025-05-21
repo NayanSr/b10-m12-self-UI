b95a26fcf9ea7512d99d1181422356c5b4ba6f41
Nayan Suter <nayankbps@gmail.com>
2025-05-21 09:46:26 +0600

    navbar added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,18 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 const Main = () => {
-    return (
-        <div className="flex flex-col justify-between min-h-screen border-2 border-red-600">
-            <h2>Header Component</h2>
-            <div className="">
-                <Outlet/>
-            </div>
-            <div>
-                <Footer/>
-            </div>
+  return (
+    <div className="flex flex-col justify-between min-h-screen border-2 border-red-600">
+      <div>
+        <Navbar/>
+        <div className="">
+          <Outlet />
         </div>
-    );
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import { FaAngleDoubleDown } from "react-icons/fa";
+
+const Navbar = () => {
+  const navOptions = (
+    <>
+      <li>
+        <a>Item 1</a>
+      </li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="p-2">
+            <li>
+              <a>Submenu 1</a>
+            </li>
+            <li>
+              <a>Submenu 2</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <a>Item 3</a>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-slate-700 shadow-sm text-white">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <FaAngleDoubleDown />
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {navOptions}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-black text-lg font-semibold">{navOptions}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
